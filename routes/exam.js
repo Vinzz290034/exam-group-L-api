73c5a74a8fc565b2ae34dcafb63d1a083a350402
unknown <YY240@ccs.local>
2025-03-26 21:01:41 +0800

    feat: add GET and exams endpoint

--- routes/exam.js
+++ routes/exam.js
@@ -1,4 +1,4 @@
-// # Modify routes/exams.js:
+// Modify routes/exams.js:
 
 const express = require('express');
 const router = express.Router();
@@ -13,3 +13,5 @@ router.get('/', (req, res) => {
 });
 
 module.exports = router;
+
+// Providing a pulling request
